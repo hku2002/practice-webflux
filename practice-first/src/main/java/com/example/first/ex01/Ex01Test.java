1371a2b6e8e3f646032a988dd1d4f38f661eeb34
Hwang <hku2002@hanmail.net>
2023-10-26 22:20:10 +0900

    refactor: 영문으로 변경

--- practice-first/src/main/java/com/example/first/ex01/Ex01Test.java
+++ practice-first/src/main/java/com/example/first/ex01/Ex01Test.java
@@ -6,13 +6,13 @@ import reactor.core.publisher.Flux;
 @Slf4j
 public class Ex01Test {
     public static void main(String[] args) {
-        /* Flux.just("Hello", "Reactor") 에서 Flux 는 생산자
+        /* Flux.just("Hello", "Reactor") 에서 Flux 는 Publisher
          * "Hello", "Reactor" 는 데이터 소스
          * .just() 는 시퀀스
          */
         Flux<String> sequence = Flux.just("Hello", "Reactor");
 
-        /* 아래 코드에서 log::info 부분이 소비자
+        /* 아래 코드에서 log::info 부분이 Subscriber
          * .map() 은 연산자
          */
         sequence.map(String::toLowerCase)
