981de3352559118c2ebbab415fc087184b1daed0
Hwang <hku2002@hanmail.net>
2023-10-26 22:01:58 +0900

    feat: Hello Reactor!

--- practice-first/src/main/java/com/example/first/HelloReactor.java
+++ practice-first/src/main/java/com/example/first/HelloReactor.java
@@ -1,4 +1,12 @@
 package com.example.first;
 
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+@Slf4j
 public class HelloReactor {
+    public static void main(String[] args) {
+        Mono.just("Hello reactor")
+                .subscribe(message -> log.info("Hello Reactor!"));
+    }
 }
