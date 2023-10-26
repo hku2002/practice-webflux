7136ef83c7a4586922eaed6c999dc98534bc50ef
Hwang <hku2002@hanmail.net>
2023-10-26 22:21:52 +0900

    refactor: 영문 변경

--- practice-first/src/main/java/com/example/first/ex01/Ex01Test.java
+++ practice-first/src/main/java/com/example/first/ex01/Ex01Test.java
@@ -8,12 +8,12 @@ public class Ex01Test {
     public static void main(String[] args) {
         /* Flux.just("Hello", "Reactor") 에서 Flux 는 Publisher
          * "Hello", "Reactor" 는 데이터 소스
-         * .just() 는 시퀀스
+         * .just() 는 Sequence
          */
         Flux<String> sequence = Flux.just("Hello", "Reactor");
 
         /* 아래 코드에서 log::info 부분이 Subscriber
-         * .map() 은 연산자
+         * .map() 은 연산자(Operator)
          */
         sequence.map(String::toLowerCase)
                 .subscribe(log::info);
