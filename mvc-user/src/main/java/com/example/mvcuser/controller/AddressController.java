280a5c3cd264f3548e6311913925cb0d29e9d959
hku2002 <hku2002@hanmail.net>
2023-12-24 20:54:59 +0900

    feat: 주소조회 controller 추가

---
+++ mvc-user/src/main/java/com/example/mvcuser/controller/AddressController.java
@@ -0,0 +1,23 @@
+package com.example.mvcuser.controller;
+
+import com.example.mvcuser.dto.AddressResponseDto;
+import com.example.mvcuser.global.common.response.BaseResponse;
+import com.example.mvcuser.global.common.response.CommonResponse;
+import com.example.mvcuser.service.AddressService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AddressController {
+
+    private final AddressService addressService;
+
+    @GetMapping("/user/address/{userId}")
+    public ResponseEntity<BaseResponse<AddressResponseDto>> getAddress(@PathVariable Long userId) {
+        return CommonResponse.success(addressService.getDefaultAddress(userId));
+    }
+}
