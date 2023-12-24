49b004ad5acec19fc1f0bea545d0d03c5e01c577
hku2002 <hku2002@hanmail.net>
2023-12-24 18:56:28 +0900

    feat: 가게 노출 API 최초 추가

---
+++ mvc-store-display/src/test/java/com/example/mvcstoredisplay/MvcStoreDisplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mvcstoredisplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcStoreDisplayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
