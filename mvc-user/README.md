483e445a5483b46367ebb4da59bab4d7ca9a0b27
hku2002 <hku2002@hanmail.net>
2023-12-24 20:12:06 +0900

    feat: user api 서버 최초 추가

---
+++ mvc-user/README.md
@@ -0,0 +1,4 @@
+### 유저 API 서버
+
+- user와 관련된 데이터를 조회하는 API 서버
+- Spring mvc
---
+++ mvc-user/src/main/java/com/example/mvcuser/MvcUserApplication.java
@@ -0,0 +1,13 @@
+package com.example.mvcuser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcUserApplication.class, args);
+    }
+
+}
