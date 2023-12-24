2bf47c4084b658f5d368a8fcbaabb0a656aadb04
hku2002 <hku2002@hanmail.net>
2023-12-24 20:20:25 +0900

    feat: user entity 최초 추가

---
+++ mvc-user/src/main/java/com/example/mvcuser/domain/entity/User.java
@@ -0,0 +1,33 @@
+package com.example.mvcuser.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String password; // TODO hash 암호화
+    private String name;
+    private String grade;
+    private int orderCount;
+
+    @Builder
+    public User(Long id, String email, String password, String name, String grade, int orderCount) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.grade = grade;
+        this.orderCount = orderCount;
+    }
+}
