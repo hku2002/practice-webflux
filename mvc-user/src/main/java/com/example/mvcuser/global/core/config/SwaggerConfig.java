783119d43abe7b5c52c1be6c32b2ef997514f8f2
hku2002 <hku2002@hanmail.net>
2023-12-24 23:15:45 +0900

    config: swagger 추가

---
+++ mvc-user/src/main/java/com/example/mvcuser/global/core/config/SwaggerConfig.java
@@ -0,0 +1,23 @@
+package com.example.mvcuser.global.core.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebMvc
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openApi() {
+        return new OpenAPI()
+                .info(
+                        new Info().title("user API 서버")
+                                .version("v1.0")
+                                .description("user API 명세서")
+                );
+    }
+
+}
