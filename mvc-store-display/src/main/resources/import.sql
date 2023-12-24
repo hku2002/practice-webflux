da40f251f74f7ac34e8dcf1a1b5d77827fb8f1ed
hku2002 <hku2002@hanmail.net>
2023-12-24 20:04:57 +0900

    config: h2 database 설정 추가

---
+++ mvc-store-display/src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO display_store (name, thumbnail_path, star_rating, delivery_fee) VALUES ('가게명1', 'http://localhost:8080/static/img1.jpg', 9, 3000);
+INSERT INTO display_store (name, thumbnail_path, star_rating, delivery_fee) VALUES ('가게명2', 'http://localhost:8080/static/img2.jpg', 7, 2500);
+INSERT INTO display_store (name, thumbnail_path, star_rating, delivery_fee) VALUES ('가게명3', 'http://localhost:8080/static/img3.jpg', 8, 3000);
+INSERT INTO display_store (name, thumbnail_path, star_rating, delivery_fee) VALUES ('가게명4', 'http://localhost:8080/static/img4.jpg', 10, 2000);
+INSERT INTO display_store (name, thumbnail_path, star_rating, delivery_fee) VALUES ('가게명5', 'http://localhost:8080/static/img5.jpg', 5, 4000);
