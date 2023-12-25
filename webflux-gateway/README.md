a0b220632f50e80b649460e3dca83e474b38c050
hku2002 <hku2002@hanmail.net>
2023-12-25 13:16:17 +0900

    feat: webflux gateway 최초 추가

---
+++ webflux-gateway/README.md
@@ -0,0 +1,4 @@
+### Webflux gateway
+
+- Client 에서 호출하는 gateway 역할
+- 각 서비스의 정보를 가져와서 조합하는 역할
---
+++ webflux-gateway/src/main/java/com/example/webfluxgateway/WebfluxGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.webfluxgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxGatewayApplication.class, args);
+    }
+
+}
