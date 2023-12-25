27b3ad5585312384b38422f0c3c86d4e161b8804
hku2002 <hku2002@hanmail.net>
2023-12-25 15:16:16 +0900

    fix: 쿼리 오류 수정

--- mvc-user/src/main/java/com/example/mvcuser/domain/repository/DeliveryAddressRepository.java
+++ mvc-user/src/main/java/com/example/mvcuser/domain/repository/DeliveryAddressRepository.java
@@ -1,6 +1,7 @@
 package com.example.mvcuser.domain.repository;
 
 import com.example.mvcuser.domain.entity.DeliveryAddress;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -8,7 +9,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DeliveryAddressRepository extends JpaRepository<DeliveryAddress, Long> {
 
-    @Query("SELECT da FROM DeliveryAddress da JOIN FETCH da.user u WHERE u.id =: userId")
-    DeliveryAddress findDefaultAddress(Long userId);
+    @Query("SELECT da FROM DeliveryAddress da JOIN FETCH da.user u WHERE u.id = :userId AND da.isDefault = true")
+    DeliveryAddress findFirstDeliveryAddress(Long userId, PageRequest pageRequest);
 
 }
--- mvc-user/src/main/java/com/example/mvcuser/service/AddressService.java
+++ mvc-user/src/main/java/com/example/mvcuser/service/AddressService.java
@@ -3,6 +3,7 @@ package com.example.mvcuser.service;
 import com.example.mvcuser.domain.repository.DeliveryAddressRepository;
 import com.example.mvcuser.dto.AddressResponseDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,6 +13,6 @@ public class AddressService {
     private final DeliveryAddressRepository deliveryAddressRepository;
 
     public AddressResponseDto getDefaultAddress(Long userId) {
-        return AddressResponseDto.createInstance(deliveryAddressRepository.findDefaultAddress(userId));
+        return AddressResponseDto.createInstance(deliveryAddressRepository.findFirstDeliveryAddress(userId, PageRequest.of(0, 1)));
     }
 }
