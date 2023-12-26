690c85ed066b7e77dd9bff6d0e9bc7527f24ca3c
hku2002 <hku2002@hanmail.net>
2023-12-26 16:34:48 +0900

    refactor: GET method 만 받도록 개선

--- webflux-gateway/src/main/java/com/example/webfluxgateway/controller/HomeController.java
+++ webflux-gateway/src/main/java/com/example/webfluxgateway/controller/HomeController.java
@@ -3,8 +3,8 @@ package com.example.webfluxgateway.controller;
 import com.example.webfluxgateway.dto.HomeResponseDto;
 import com.example.webfluxgateway.service.storedisplay.HomeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
@@ -14,7 +14,7 @@ public class HomeController {
 
     private final HomeService homeService;
 
-    @RequestMapping("/home/display/users/{userId}")
+    @GetMapping("/home/display/users/{userId}")
     public Mono<HomeResponseDto> getHomeItems(@PathVariable int userId) {
         return homeService.getDisplayStore(userId);
     }
