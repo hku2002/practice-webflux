aa9a57ebb9470eeae32fa9f2ff53943c5feab646
hku2002 <hku2002@hanmail.net>
2024-03-08 22:34:50 +0900

    feat: swithIfEmpty를 활용한 defer 연습

---
+++ practice-operator/src/main/java/com/example/operator/create/DeferSwitchIfEmpty01.java
@@ -0,0 +1,32 @@
+package com.example.operator.create;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+
+@Slf4j
+public class DeferSwitchIfEmpty01 {
+
+    public static void main(String[] args) throws InterruptedException {
+        Mono.just("Good morning! v1")
+                .delayElement(Duration.ofSeconds(2L))
+                .switchIfEmpty(sayHello())
+                .subscribe(result -> log.info("result v1(no defer) : {}", result));
+        Thread.sleep(2500L);
+
+        log.info("====================");
+
+        Mono.just("Good morning! v2")
+                .delayElement(Duration.ofSeconds(2L))
+                .switchIfEmpty(Mono.defer(DeferSwitchIfEmpty01::sayHello))
+                .subscribe(result -> log.info("result v2(defer) : {}", result));
+        Thread.sleep(2500L);
+    }
+
+    public static Mono<String> sayHello() {
+        log.info("sayHello method called!");
+        return Mono.just("Hello!");
+    }
+
+}
