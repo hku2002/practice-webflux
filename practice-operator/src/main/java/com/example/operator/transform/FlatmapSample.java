87f53e03174c189733235a58265de82c37883ed3
hku2002 <hku2002@hanmail.net>
2024-03-14 21:43:05 +0900

    feat: flatmap operator 추가

---
+++ practice-operator/src/main/java/com/example/operator/transform/FlatmapSample.java
@@ -0,0 +1,17 @@
+package com.example.operator.transform;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+@Slf4j
+public class FlatmapSample {
+
+    public static void main(String[] args) {
+        Flux.just("A", "B", "C")
+                .flatMap(alphabet ->
+                        Flux.just("Hello", "World")
+                )
+                .subscribe(result -> log.info("result: {}", result));
+    }
+
+}
