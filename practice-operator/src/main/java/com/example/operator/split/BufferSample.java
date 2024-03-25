398377af57f96f641fe14a36d0d33c052a977f63
hku2002 <hku2002@hanmail.net>
2024-03-25 21:52:49 +0900

    feat: bufferTimeout operator 추가

--- practice-operator/src/main/java/com/example/operator/split/BufferSample.java
+++ practice-operator/src/main/java/com/example/operator/split/BufferSample.java
@@ -3,6 +3,8 @@ package com.example.operator.split;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
+
 @Slf4j
 public class BufferSample {
 
@@ -12,6 +14,23 @@ public class BufferSample {
                 .buffer(10)
                 .subscribe(result -> log.info("result: {}", result));
 
+
+        Flux
+                .range(1, 20)
+                .mapNotNull(num -> {
+                    try {
+                        if (num < 10) {
+                            Thread.sleep(100L);
+                        } else {
+                            Thread.sleep(300L);
+                        }
+                    } catch (Exception e) {
+                        log.error("exception: ", e);
+                    }
+                    return num;
+                })
+                .bufferTimeout(3, Duration.ofMillis(400L))
+                .subscribe(result -> log.info("result: {}", result));
     }
 
 }
